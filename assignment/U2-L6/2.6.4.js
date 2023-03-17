d7db5c0ffaa9e16510d83bbc861be569364f7a19
GaryZhang123 <ztrgary@outlook.com>
2023-03-17 22:29:54 +0800

    1

---
+++ assignment/U2-L6/2.6.4.js
@@ -0,0 +1,13 @@
+nextPrime:
+
+    for (let i=2; i<10; i++) {
+        for (let y=2;y<i;y++) {
+            if (i%y===0) {
+                nextPrime;
+                continue
+
+
+            }
+        }
+        console.log(i)
+        }
